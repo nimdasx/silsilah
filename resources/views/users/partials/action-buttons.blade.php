6b5e9a635f92fea3cac1da48298f5a5c5e3fa5ac
Sofyan <nimdasx@gmail.com>
2024-03-18 15:12:24 +0700

    bugfix button active state on show family tree with couples

--- resources/views/users/partials/action-buttons.blade.php
+++ resources/views/users/partials/action-buttons.blade.php
@@ -5,7 +5,7 @@
     {{ link_to_route('users.show', trans('app.show_profile').' '.$user->name, [$user->id], ['class' => Request::segment(3) == null ? 'btn btn-default active' : 'btn btn-default']) }}
     {{ link_to_route('users.chart', trans('app.show_family_chart'), [$user->id], ['class' => Request::segment(3) == 'chart' ? 'btn btn-default active' : 'btn btn-default']) }}
     {{ link_to_route('users.tree', trans('app.show_family_tree'), [$user->id], ['class' => Request::segment(3) == 'tree' ? 'btn btn-default active' : 'btn btn-default']) }}
-    {{ link_to_route('users.tree-couples', trans('app.show_family_tree_with_couples'), [$user->id], ['class' => Request::segment(3) == 'tree-couple' ? 'btn btn-default active' : 'btn btn-default']) }}
+    {{ link_to_route('users.tree-couples', trans('app.show_family_tree_with_couples'), [$user->id], ['class' => Request::segment(3) == 'tree-couples' ? 'btn btn-default active' : 'btn btn-default']) }}
     {{ link_to_route('users.marriages', trans('app.show_marriages'), [$user->id], ['class' => Request::segment(3) == 'marriages' ? 'btn btn-default active' : 'btn btn-default']) }}
     @if ($user->yod)
         {{ link_to_route('users.death', trans('user.death'), [$user->id], ['class' => Request::segment(3) == 'death' ? 'btn btn-default active' : 'btn btn-default']) }}
